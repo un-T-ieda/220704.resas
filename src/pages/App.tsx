20b51074f1969fbc115ded1bd13b85c253eb1131
Ieda Shinsuke <shinsuke.ieda@un-t.com>
2022-05-06 02:15:18 +0900

    add: mockapi

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+type Result = {
+  prefCode: number;
+  prefName: string;
+};
+
+const isProduction = import.meta.env.MODE === 'production';
+const prefecturesAPIUrl = isProduction
+  ? 'https://opendata.resas-portal.go.jp/api/v1/prefectures'
+  : 'http://localhost:8000/prefectures';
+
+const fetchData = async (url = '') => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': '',
+    },
+  });
+  return response.json();
+};
 
 export const App = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [data, _setData] = useState<string>('sample');
+  const [data, setData] = useState<Result[]>([]);
 
-  return <p>{data}</p>;
+  useEffect(() => {
+    fetchData(prefecturesAPIUrl).then((res) => {
+      const data: Result[] = res;
+
+      setData(data);
+    });
+  }, []);
+
+  return <div>{JSON.stringify(data)}</div>;
 };
