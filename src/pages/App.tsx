c296b77f8bd6066e1f4132cf75b60a6e4409da82
Ieda Shinsuke <shinsuke.ieda@un-t.com>
2022-05-06 13:09:58 +0900

    feat: add check prefectures

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import { useEffect, useState, ChangeEvent } from 'react';
 
 type Result = {
   prefCode: number;
@@ -23,14 +24,56 @@ const fetchData = async (url = '') => {
 
 export const App = () => {
   const [data, setData] = useState<Result[]>([]);
+  const [checkList, setCheckList] = useState<number[]>([]);
 
   useEffect(() => {
     fetchData(prefecturesAPIUrl).then((res) => {
-      const data: Result[] = res;
+      const data: Result[] = res.result;
 
       setData(data);
     });
   }, []);
 
-  return <div>{JSON.stringify(data)}</div>;
+  const handleChange = (event: ChangeEvent) => {
+    const eventTarget = event.target as HTMLInputElement;
+    const eventPrefCode = Number(eventTarget.value);
+
+    if (eventTarget.checked) {
+      setCheckList([...checkList, eventPrefCode]);
+    } else {
+      setCheckList(checkList.filter((item) => item !== eventPrefCode));
+    }
+  };
+
+  return (
+    <>
+      <div>{JSON.stringify(checkList)}</div>
+      <ul
+        css={css`
+          display: flex;
+          gap: 2rem;
+          flex-wrap: wrap;
+        `}
+      >
+        {data.map((item) => (
+          <li key={item.prefCode}>
+            <label
+              css={css`
+                cursor: pointer;
+              `}
+            >
+              {item.prefName}
+              <input
+                onChange={(event) => {
+                  handleChange(event);
+                }}
+                type="checkbox"
+                value={item.prefCode}
+              />
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
