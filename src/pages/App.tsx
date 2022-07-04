84e25ffe6b9bc0f084bd2b7b5535816d6ddfc4ef
Ieda Shinsuke <shinsuke.ieda@un-t.com>
2022-07-05 00:09:56 +0900

    refactor: devided useFetchResas hooks

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,69 +1,16 @@
-import { useEffect, useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent } from 'react';
 import { css } from '@emotion/react';
 import { AppChart } from '@/components/AppChart';
 import { CheckButtonList } from '@/components/CheckButtonList';
 import { LayoutContainer } from '@/components/LayoutContainer';
 import { SelectedPrefecture } from '@/components/SelectedPrefecture';
 import { mixins } from '@/utils/styles';
-import {
-  PopulationData,
-  PopulationResponse,
-  PrefecturePopulation,
-  PrefectureResponse,
-} from '@/types';
-
-const isProduction = import.meta.env.MODE === 'production';
-const prefecturesAPIUrl = isProduction
-  ? 'https://opendata.resas-portal.go.jp/api/v1/prefectures'
-  : 'http://localhost:8000/prefectures';
-const populationAPIUrl = (prefCode: number): string => {
-  return isProduction
-    ? `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`
-    : `http://localhost:8000/population?prefCode=${prefCode}`;
-};
-
-const fetchData = async (url = '') => {
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': import.meta.env.VITE_API_KEY,
-    },
-  });
-  return response.json();
-};
+import { useFetchResas } from '@/hooks/useFetchResas';
 
 export const App = () => {
-  const [prefecture, setPrefecture] = useState<PrefectureResponse[]>([]);
-  const [population, setPopulation] = useState<PrefecturePopulation[]>([]);
   const [checkList, setCheckList] = useState<number[]>([]);
-
-  const fetchPrefectures = () => {
-    fetchData(prefecturesAPIUrl).then((res) => {
-      const data: PrefectureResponse[] = res.result;
-
-      setPrefecture(data);
-    });
-  };
-
-  const fetchPopulation = (eventPrefCode: number) => {
-    fetchData(populationAPIUrl(eventPrefCode)).then(
-      (res: PopulationResponse) => {
-        const data: PopulationData[] = res.result.data[0].data;
-        const prefName = prefecture.find(
-          (item) => item.prefCode === eventPrefCode,
-        )!.prefName;
-
-        setPopulation([
-          ...population,
-          {
-            prefName,
-            data,
-          },
-        ]);
-      },
-    );
-  };
+  const { prefecture, population, fetchPopulation, setPopulation } =
+    useFetchResas();
 
   const handleChange = (event: ChangeEvent) => {
     const eventTarget = event.target as HTMLInputElement;
@@ -80,10 +27,6 @@ export const App = () => {
       setPopulation(population.filter((item) => item.prefName !== prefName));
     }
   };
-
-  useEffect(() => {
-    fetchPrefectures();
-  }, []);
 
   return (
     <LayoutContainer>
